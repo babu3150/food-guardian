dc835fcb9cb6e6c7a29a930a0b6cc263ea42932f
babu <hiroto.moriyama7204@gmail.com>
2026-05-07 00:21:49 +0900

    食材一覧コンポーネントの作成

---
+++ frontend/src/Pages/HomePage.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react";
+// 食材の一覧リストのコンポーネント
+import FoodList from '../components/FoodList';
+// 食材追加フォームのコンポーネント
+import FoodForm from '../components/FoodForm';
+
+function HomePage({ onLogout }) {
+    // 食材管理用ステート
+    const [foods, setFoods] = useState([]);
+
+    // コンポーネントのマウント後に食材の一覧リストを取得（第2引数を空にすることで初回レンダリング時のみ実行）
+    useEffect(() => {
+        fetchFoods();
+    }, []);
+
+    // 食材の一覧リストを取得
+    const fetchFoods = async () => {
+        const response = await fetch('/api/foods');
+        if (response.ok) {
+            const data = await response.json();
+            setFoods(data);
+        }
+    };
+
+    // 食材の追加
+    const handleAddFood = async (food) => {
+        const response = await fetch('/api/foods', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(food)
+        });
+
+        if (response.ok) {
+            fetchFoods();
+        }
+    };
+
+    // 食材の削除
+    const handleDeleteFood = async (id) => {
+        const response = await fetch(`/api/foods/${id}`, {
+            method: 'DELETE'
+        });
+
+        if (response.ok) {
+            // 削除した食材は一覧リストから除外
+            setFoods(foods.filter(food => food.id !== id));
+        }
+    };
+
+    return (
+        <div>
+            <h2>食材の番人</h2>
+            <button onClick={onLogout}>ログアウト</button>
+            <hr />
+            <h3>食材の追加</h3>
+            <FoodForm onAddFood={handleAddFood} />
+            <hr />
+            <h3>食材の一覧リスト</h3>
+            <FoodList foods={foods} onDeleteFood={handleDeleteFood} />
+        </div>
+    );
+}
+
+export default HomePage;

635cadc10d93d86fea21048d9fcd8d969687d9a3
babu <hiroto.moriyama7204@gmail.com>
2026-05-09 05:47:20 +0900

    食材項目のコンポーネント作成

---
+++ frontend/src/components/FoodItem.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+// 食材の項目
+function FoodItem({ food, onDelete }) {
+    return (
+        <div className="food-item">
+            {/* ここはfood.nameとなるよう修正する */}
+            <h3>{food.food}</h3>
+            <p>{food.expiration_date}</p>
+            <p>{food.quantity}</p>
+            <button className="delete" onClick={onDelete}>削除</button>
+        </div>
+    );
+}
+
+export default FoodItem;

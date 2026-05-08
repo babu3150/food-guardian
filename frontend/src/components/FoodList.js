943780e931908633b4d4fc53e027c87815369bf5
babu <hiroto.moriyama7204@gmail.com>
2026-05-09 05:32:18 +0900

    食材リストのコンポーネント作成

---
+++ frontend/src/components/FoodList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import FoodItem from './FoodItem';
+
+function FoodList({ foods, onDeleteFood }) {
+    return (
+        <div>
+            {foods.map((food) => {
+                <FoodItem key={food.id} food={food} onDelete={() => onDeleteFood(food.id)} />
+            })}
+        </div>
+    );
+}
+
+export default FoodList;

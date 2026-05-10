c12467aca3d8ae1e94beb8b811e62cc9f5cc3b28
babu <hiroto.moriyama7204@gmail.com>
2026-05-10 19:50:53 +0900

    ログインページのレイアウト変更

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -4,9 +4,14 @@ import LoginForm from '../components/LoginForm';
 
 function LoginPage({ onLogin }) {
     return (
-        <div>
-            <h2>食材の番人</h2>
-            <LoginForm onLogin={ onLogin } />
+        <div className="login-page">
+            <div className="login-form-area">
+                <h2>食材の番人</h2>
+                <LoginForm onLogin={ onLogin } />
+            </div>
+            <div className="login-image-area">
+                <img src="/food-guardian.png" alt="食材の番人" className="food-guardian-image" />
+            </div>
         </div>
     );
 }

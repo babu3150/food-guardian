6fc93afcc5a8c1f4c17fd6f5a68989b5ea6c6b24
babu <hiroto.moriyama7204@gmail.com>
2026-05-27 21:21:18 +0900

    ユーザー登録フォームのプレースホルダーの名称を変更

--- frontend/src/components/SignupForm.js
+++ frontend/src/components/SignupForm.js
@@ -27,8 +27,8 @@ function SignupForm( { onSignup } ) {
 
     return (
         <form onSubmit={handleSubmit}>
-            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} onInvalid={(e) => {e.target.setCustomValidity('そなたの名を入力せよ')}} onInput={(e) => {e.target.setCustomValidity('')}} placeholder="扉を開く者" required />
-            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} onInvalid={(e) => {e.target.setCustomValidity('そなたの暗号を入力せよ')}} onInput={(e) => {e.target.setCustomValidity('')}} placeholder="扉を開く暗号" required />
+            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} onInvalid={(e) => {e.target.setCustomValidity('そなたの名を入力せよ')}} onInput={(e) => {e.target.setCustomValidity('')}} placeholder="貴殿の名" required />
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} onInvalid={(e) => {e.target.setCustomValidity('そなたの暗号を入力せよ')}} onInput={(e) => {e.target.setCustomValidity('')}} placeholder="貴殿の暗号" required />
             <button type="submit" className="signup">冷蔵庫の主となる</button>
             {error && <p>{error}</p>}
         </form>
